8ed02c46cb0083f0e3c1190dd4299b33463ac380
taidopurason <taido.purason@ut.ee>
2025-09-15 09:11:28 +0300

    Updated mixed dataset creation to match regular dataset creation

--- experiments/create_dataset_mixed.py
+++ experiments/create_dataset_mixed.py
@@ -25,15 +25,11 @@ def get_fineweb_ds(lang="ekk_Latn", streaming=True):
         extra_shuffle_args = {"buffer_size": 1000}
 
     if lang == "eng_Latn":
-        ds = load_dataset(
-            "HuggingFaceFW/fineweb", "sample-10BT", split="train", streaming=streaming
-        ).shuffle(seed=42, **extra_shuffle_args).skip(10000)
+        ds = load_dataset("HuggingFaceFW/fineweb", "sample-10BT", split="train", streaming=streaming)
     else:
-        ds = load_dataset(
-            "HuggingFaceFW/fineweb-2", lang, split="train", streaming=streaming
-        ).shuffle(seed=42, **extra_shuffle_args)
-    return ds
+        ds = load_dataset("HuggingFaceFW/fineweb-2", lang, split="train", streaming=streaming)
 
+    return ds.shuffle(seed=42, **extra_shuffle_args).skip(10000)
 
 
 def get_codeparrot_ds(lang="all-all", streaming=True):
@@ -46,6 +42,7 @@ def get_codeparrot_ds(lang="all-all", streaming=True):
     ).rename_column("code", "text")
 
     return ds.shuffle(seed=42, **extra_shuffle_args).skip(10000)
+
 
 def get_ds(dataset, lang, streaming):
     if dataset == "culturax":
@@ -92,7 +89,6 @@ def create_budget_ds(
     if len(langs) > 1:
         logging.info("Shuffling training data")
         random.Random(42).shuffle(training_data)
-
 
     logging.info(f"Saving dataset to {output_dir}")
     ds = Dataset.from_list(training_data)
